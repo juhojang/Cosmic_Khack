ad60197dcd01912c4d95bead860a9814387918bd
juhojang <juhojang@hotmail.com>
2022-08-01 19:20:04 +0900

    쇼이미지 실습코드

---
+++ showImage_example.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(showImage());
+}
+
+class showImage extends StatefulWidget {
+
+  @override
+  State<showImage> createState() => _showImageState();
+}
+
+class _showImageState extends State<showImage> {
+
+
+  Widget showImage() {
+    return Container(
+      color: const Color(0xffd0cece),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: showImage(),
+    );
+  }
+}
+class _showImageState extends State<showImage> {
+
+
+  Widget showImage() {
+    return Container(
+      color: const Color(0xffd0cece),
+      width: 100,
+      height: 100,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: showImage(),
+      ),
+    );
+  }
+}
